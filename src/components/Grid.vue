c9c1e131f6ad18798b8bf0cf8ed7e0a0f5c3694c
husseinTalal2 <hossientalal2@gmail.com>
2021-10-29 19:26:01 +0300

    adding posts grid

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,19 +1,30 @@
 <template>
   <ag-grid-vue
-    style="width: 500px; height: 500px"
-    class="ag-theme-alpine"
+    style="width: 60vw; height: 80vh"
+    class="ag-theme-alpine-dark"
     :columnDefs="columnDefs"
     :rowData="rowData"
+    :paginationAutoPageSize="true"
     :pagination="true"
-  >
-  </ag-grid-vue>
+    :rowSelection="rowSelection"
+    @grid-ready="onGridReady"
+    @selection-changed="onSelectionChanged"
+  ></ag-grid-vue>
 </template>
 
-<script>
+<script lang="ts">
 import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
 import { AgGridVue } from "ag-grid-vue3";
 import { reactive, onMounted } from "vue";
+import router from "@/router";
+
+interface Row {
+  title: string;
+  id: number;
+  userId: number;
+  body: string;
+}
 
 export default {
   name: "Grid",
@@ -27,20 +38,57 @@ export default {
       fetch("https://jsonplaceholder.typicode.com/posts")
         .then((result) => result.json())
         .then((remoteRowData) => {
-          console.log(remoteRowData);
-          remoteRowData.forEach((row) => {
-            rowData.push({ ID: row.id, userId: row.userId, title: row.title });
+          remoteRowData.forEach((row: Row) => {
+            const rowDataValues = {
+              ID: row.id,
+              userId: row.userId,
+              title: row.title,
+              rowStyles: { width: "100%" },
+            };
+            rowData.push(rowDataValues);
           });
         });
     });
     return {
       columnDefs: [
-        { headerName: "ID", field: "ID" },
-        { headerName: "UserId", field: "userId" },
-        { headerName: "Title", field: "title" },
+        {
+          headerName: "ID",
+          field: "ID",
+          cellStyle: { "text-align": "left" },
+          flex: 1,
+          cellRenderer: function (params: { value: string }) {
+            return `<div v-on:click="router.push({ name: 'post', params: { userId: '123' } })">${params.value}</div>`;
+          },
+        },
+        {
+          headerName: "UserId",
+          field: "userId",
+          cellStyle: { "text-align": "left" },
+          flex: 1,
+        },
+        {
+          headerName: "Title",
+          field: "title",
+          cellStyle: { "text-align": "left" },
+          flex: 3,
+        },
       ],
       rowData,
+      rowSelection: "single",
+      gridApi: null,
+      columnApi: null,
     };
+  },
+
+  methods: {
+    onSelectionChanged() {
+      const selectedRows = this.gridApi.getSelectedRows();
+      router.push({ name: "Post", params: { id: selectedRows[0].ID } });
+    },
+    onGridReady(params: any) {
+      this.gridApi = params.api;
+
+    },
   },
 };
 </script>
