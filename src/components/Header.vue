1ea23307a9bf0638fe8c142d92b9aec85ee03a9c
husseinTalal2 <hossientalal2@gmail.com>
2021-10-29 19:26:14 +0300

    adding header component

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>BLOG POSTS</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+});
+</script>
+
+<style scoped lang="scss">
+header {
+  margin-bottom: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4rem;
+  background-color: #f2f8f8;
+}
+</style>
