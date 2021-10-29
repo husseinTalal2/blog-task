9d61c2eb0b64927c3c75eb89a32c1ec96a4a7b14
husseinTalal2 <hossientalal2@gmail.com>
2021-10-29 19:27:19 +0300

    adding header and grid tests

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,21 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
+import Grid from "@/components/Header.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+describe("Header.vue", () => {
+  it("renders Header and contains 'BLOG POST' string", () => {
+    const msg = "BLOG POST";
+    const wrapper = shallowMount(Header, {
+      props: {},
     });
     expect(wrapper.text()).to.include(msg);
+  });
+});
+describe("Grid.vue", () => {
+  it("renders the grid", () => {
+    shallowMount(Grid, {
+      props: {},
+    });
   });
 });
